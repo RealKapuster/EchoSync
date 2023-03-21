9677b9fc07c675255cf703a971ee171ea5e1618d
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2023-03-21 20:50:32 +0900

    add gpt api

--- src/pages/api-call-example.tsx
+++ src/pages/api-call-example.tsx
@@ -1,7 +1,17 @@
 import React, { useRef, useState } from "react";
 import axios, { AxiosError } from "axios";
-import { Button, Form, message, Spin, Typography } from "antd";
+import {
+  Button,
+  Form,
+  message,
+  Radio,
+  RadioChangeEvent,
+  Spin,
+  Typography,
+} from "antd";
 import { UploadOutlined } from "@ant-design/icons";
+import { PromptType } from "./api/gpt";
+import { ChatCompletionResponseMessage } from "openai";
 
 const { Text } = Typography;
 
@@ -9,7 +19,8 @@ const ApiCallExample = () => {
   const [file, setFile] = useState<File | null>(null);
   const [loading, setLoading] = useState(false);
   const [transcription, setTranscription] = useState<string | null>(null);
-
+  const [content, setContent] = useState<string | null>(null);
+  const [promptType, setPromptType] = useState<PromptType>("Tweet");
   const inputFileRef = useRef<HTMLInputElement>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -58,6 +69,29 @@ const ApiCallExample = () => {
 
     setLoading(false);
   };
+  const handleSubmitTranscription = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post<ChatCompletionResponseMessage>(
+        "/api/gpt",
+        { text: transcription, promptType: promptType }
+      );
+
+      console.log(response.data);
+      setContent(response.data.content);
+    } catch (error: any) {
+      const errorMessage =
+        error.response?.data?.error || "Internal Server Error";
+
+      message.error(errorMessage);
+      console.error(error);
+    }
+
+    setLoading(false);
+  };
+  const handlePromptTypeChange = (e: RadioChangeEvent) => {
+    setPromptType(e.target.value);
+  };
 
   return (
     <div
@@ -100,9 +134,52 @@ const ApiCallExample = () => {
         </div>
       )}
       {transcription && (
+        <>
+          <Form
+            onFinish={() => void handleSubmitTranscription()}
+            fields={[
+              {
+                name: ["promptType"],
+                value: promptType,
+              },
+            ]}
+          >
+            <Form.Item>
+              <div style={{ marginTop: "16px" }}>
+                <Text strong>Transcription:</Text>
+                <Text>{transcription}</Text>
+              </div>
+            </Form.Item>
+            <Form.Item className="mb-0" name="promptType">
+              <Radio.Group onChange={handlePromptTypeChange}>
+                <Radio value="Tweet">Tweet</Radio>
+                <Radio value="Blog">Blog</Radio>
+                <Radio value="Newsletter">Newsletter</Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item className="pt-0">
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!file || loading}
+                style={{ marginTop: "16px" }}
+              >
+                Ask ChatGPT
+              </Button>
+            </Form.Item>
+          </Form>
+          {loading && (
+            <div style={{ marginTop: "16px" }}>
+              <Spin size="large" />
+              <Text style={{ marginLeft: "8px" }}>Loading...</Text>
+            </div>
+          )}
+        </>
+      )}
+      {content && (
         <div style={{ marginTop: "16px" }}>
-          <Text strong>Transcription:</Text>
-          <Text>{transcription}</Text>
+          <Text strong>Content:</Text>
+          <Text className="whitespace-pre-wrap">{content}</Text>
         </div>
       )}
     </div>
